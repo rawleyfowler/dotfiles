831bd44623a86199be981e684f7a0eb6b86c3c4a
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-06-06 07:29:12 -0600

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,6 +15,8 @@
 (transient-mark-mode)
 (show-paren-mode 1)
 
+(setq backup-directory-alist '(("." . "~/.backups")))
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (require 'package)
@@ -31,9 +33,6 @@
 	use-package-expand-minimally t))
 
 (require 'use-package)
-
-;; Misc Packages
-(use-package restart-emacs)
 
 ;; Theme
 (use-package modus-themes
