7fa31f7ce27de8c9c2f877e31df1fcbe9386ecf6
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-06-08 08:11:05 -0600

    Simplify lsp with eglot

--- emacs.d/lsp.el
+++ emacs.d/lsp.el
@@ -2,20 +2,12 @@
 ;;; Commentary:
 ;;; LSP and autocomplete configurations
 ;;; Code:
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c 1")
-  :hook
-  (lsp-mode . lsp-enable-whick-key-integration)
-  :commands (lsp lsp-deferred))
+(use-package eglot)
 
 (use-package yasnippet
   :config
   (yas-global-mode))
 (use-package yasnippet-snippets)
-
-(use-package lsp-ui
-  :commands lsp-ui-mode)
 
 (use-package flycheck
   :ensure t
@@ -28,21 +20,4 @@
 
 (use-package helm-lsp)
 
-;; Java
-(use-package lsp-java
-  :config
-  (add-hook 'java-mode-hook 'lsp))
-
-(use-package dap-mode
-  :after
-  lsp-mode
-  :config
-  (dap-auto-configure-mode))
-(use-package dap-java
-  :ensure nil)
-
-;; Auto complete
-(use-package auto-complete
-  :init
-  (ac-config-default))
 ;;; lsp.el ends here
