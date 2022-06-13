0ac51f25a27ce2666ce41fda755920a3171faa5a
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-06-12 18:42:29 -0600

    Update lsp.el

--- emacs.d/lsp.el
+++ emacs.d/lsp.el
@@ -14,8 +14,13 @@
   :init
   (global-flycheck-mode))
 
+(when (eq system-type 'darwin)
+  (use-package exec-path-from-shell ; Needed for MacOS with flycheck
+    :init
+    (exec-path-from-shell-initialize)))
+
 (use-package company
-  :hook
+  :init
   (global-company-mode))
 
 (use-package helm-lsp)
