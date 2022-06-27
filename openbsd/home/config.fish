a3256f7ac04b186ea429a57f4a1c6b5d6fb5b07c
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-06-26 18:18:25 -0600

    Update config.fish

--- openbsd/home/config.fish
+++ openbsd/home/config.fish
@@ -2,3 +2,8 @@ if status is-interactive
   set -U fish_greeting
 end
 alias emacs="emacs --no-x-resources"
+
+# set the workspace path
+set -x GOPATH $HOME/.go
+# add the go bin path to be able to execute our programs
+set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
