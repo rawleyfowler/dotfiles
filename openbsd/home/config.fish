bd9d641b09ecad8eeee109d0358c4fbc4ef56c90
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-07-02 07:11:13 -0600

    Fix ruby gem path

--- openbsd/home/config.fish
+++ openbsd/home/config.fish
@@ -5,6 +5,6 @@ end
 alias emacs="emacs --no-x-resources"
 
 set -x GEM_HOME "$HOME/.local/ruby"
-set -x GEM_PATH "$HOME/.local/ruby/3.0"
+set -x GEM_PATH "$HOME/.local/ruby/gems"
 set -x GOPATH "$HOME/.go"
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin $HOME/.local/ruby/3.0/bin
