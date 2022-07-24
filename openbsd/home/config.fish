8346c500aa4e1db1ce1f1e167e7e4496ca68efe9
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-07-24 12:55:13 -0600

    Update config.fish

--- openbsd/home/config.fish
+++ openbsd/home/config.fish
@@ -2,8 +2,9 @@ if status is-interactive
 	set -U fish_greeting
 end
 
-alias emacs="emacs --no-x-resources"
-
 set -x GEM_HOME "$HOME/.local/ruby"
 set -x GOPATH "$HOME/.go"
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin $HOME/.local/ruby/bin
+
+alias vim="nvim"
+alias vi="nvim"
