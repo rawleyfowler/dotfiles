87ddda402128db664fc086cc86a8fa6cb343b165
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-07-24 20:22:53 -0600

    Create set.lua

---
+++ neovim/lua/rf/set.lua
@@ -0,0 +1,11 @@
+local set = vim.opt
+set.nu = true
+set.tabstop = 2
+set.shiftwidth = 2
+set.hlsearch = false
+set.incsearch = true
+set.smartindent = true
+set.wrap = false
+
+-- Lower update time the better (ms)
+set.updatetime = 50
