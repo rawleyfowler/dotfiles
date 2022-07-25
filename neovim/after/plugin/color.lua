8c98a880489fd50b245be5e934fd32bb33477186
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-07-24 20:23:48 -0600

    Create color.lua

---
+++ neovim/after/plugin/color.lua
@@ -0,0 +1,11 @@
+vim.opt.background = 'dark'
+vim.cmd('colorscheme cosme')
+vim.g.airline_theme = 'cosme'
+
+local cl = function(t, o)
+	vim.api.nvim_set_hl(0, t, o)
+end
+
+cl('LineNr', {
+	bg = '#444444'
+})
