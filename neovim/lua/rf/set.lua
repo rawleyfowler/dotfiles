d6c153abcec7f58a071edc2cc238f40d6a6a5baf
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-07-26 08:00:06 -0600

    Update set.lua

--- neovim/lua/rf/set.lua
+++ neovim/lua/rf/set.lua
@@ -1,4 +1,5 @@
 local set = vim.opt
+
 set.nu = true
 set.tabstop = 2
 set.shiftwidth = 2
@@ -6,6 +7,8 @@ set.hlsearch = false
 set.incsearch = true
 set.smartindent = true
 set.wrap = false
+set.encoding = 'utf-8'
+set.guifont = 'Iosevka Nerd Font Mono 14'
 
 -- Lower update time the better (ms)
 set.updatetime = 50
