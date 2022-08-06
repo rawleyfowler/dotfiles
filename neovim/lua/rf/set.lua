5dd6ee360cea8f84eef3c945114331b6e4c8e43e
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-08-06 11:33:26 -0600

    add rtp set

--- neovim/lua/rf/set.lua
+++ neovim/lua/rf/set.lua
@@ -10,5 +10,9 @@ set.wrap = false
 set.encoding = 'utf-8'
 set.guifont = 'Iosevka Nerd Font Mono 14'
 
+vim.cmd([[
+set rtp^="home/rf/.opam/default/share/ocp-indent/vim"
+]])
+
 -- Lower update time the better (ms)
 set.updatetime = 50
